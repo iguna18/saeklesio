ab25b74fd2725bcfd1f943805d8b080d52fd752e
iguna18 <iguna18@freeuni.edu.ge>
2023-08-04 23:33:34 +0400

    developing server

--- server/server.js
+++ server/server.js
@@ -1,14 +1,33 @@
 const express = require('express');
+const axios = require('axios');
+const cheerio = require('cheerio');
+
 const app = express();
-const port = 3000; // You can change this to any port you prefer
 
-// Define a route
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+app.use(express.static('build'))
+app.use(express.json())
+
+app.get('/api/goc', async (req, res) => {
+  console.log(req.query.month, req.query.day)
+  return res.end()
+  try {
+    const url = 'https://www.orthodoxy.ge/calendar/2023/v2/01/0101.htm';
+    const response = await axios.get(url);
+    const html = response.data;
+    
+    // Use Cheerio to parse the HTML
+    const $ = cheerio.load(html);
+    const firstParagraph = $('p').first().text();
+    
+    console.log(firstParagraph);
+    res.send(`Content of the first <p> element: ${firstParagraph}`);
+  } catch (error) {
+    console.error('Error:', error.message);
+    res.status(500).send('Something went wrong.');
+  }
 });
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server started on http://localhost:${PORT}`);
 });
-
