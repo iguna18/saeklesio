7098dcf067dc9bc147b74dc2c95ffb5b8adc82a4
iguna18 <iguna18@freeuni.edu.ge>
2023-08-08 23:33:21 +0400

    server works as intended

--- server/server.js
+++ server/server.js
@@ -9,18 +9,40 @@ app.use(express.json())
 
 app.get('/api/goc', async (req, res) => {
   console.log(req.query.month, req.query.day)
-  return res.end()
+  if(!req.query.month || !req.query.day) {
+    return res.status(400).json({ error: 'Missing required parameters.' });
+  }
+
   try {
-    const url = 'https://www.orthodoxy.ge/calendar/2023/v2/01/0101.htm';
+    const url = `https://www.orthodoxy.ge/calendar/2023/v2/
+${req.query.month}/${req.query.day}${req.query.month}.htm`;
+    console.log(url);
     const response = await axios.get(url);
     const html = response.data;
     
     // Use Cheerio to parse the HTML
     const $ = cheerio.load(html);
-    const firstParagraph = $('p').first().text();
+
+    // Find the table with the specified ID
+    const autoNumber1Table = $('#AutoNumber1');
+
+    // Get the content of the body after the table
+    // const bodyContentAfterTable = autoNumber1Table.nextAll('').html();
+    const bodyClosingTag = $('body').children().last();
+
+    let contentBetweenElements = '';
     
-    console.log(firstParagraph);
-    res.send(`Content of the first <p> element: ${firstParagraph}`);
+    autoNumber1Table.nextUntil('</body>').each((index, element) => {
+      contentBetweenElements += $(element).toString();
+    });
+    console.log(contentBetweenElements);
+    res.send(`Content of <body> after <table id="AutoNumber1">: ${contentBetweenElements}`);
+
+    // const firstParagraph = $('p').first().text();
+    
+    // console.log(firstParagraph);
+    // // res.send(`Content of the first <p> element: ${firstParagraph}`);
+    // res.json({message:`Content of the first <p> element: ${firstParagraph}`});
   } catch (error) {
     console.error('Error:', error.message);
     res.status(500).send('Something went wrong.');
